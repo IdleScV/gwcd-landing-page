1f0ec9319cea6c7c3616b25e151e691deea96554
IdleScV <weschen1996@gmail.com>
2023-10-13 11:28:37 -0400

    WIP wwo page

--- src/app/what-we-offer/page.tsx
+++ src/app/what-we-offer/page.tsx
@@ -1,65 +1,91 @@
-import { Stack, Box, Text, Image } from "@chakra-ui/react";
+import { Stack, Box, Text, Image, HStack } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function WhatWeOffer() {
     return (
-        <Stack
-            backgroundColor="brand.pageBackground"
-            alignItems={"center"}
-            spacing={16}
-        >
+        <Stack backgroundColor="brand.pageBackground" alignItems={"center"}>
             <Stack width="100%" spacing={0}>
                 <Box
                     position="absolute"
                     height="225px"
-                    maxWidth="800px"
-                    left="50%"
-                    top="35%"
-                    transform="translate(-50%, -15vh)"
-                    width="100%"
+                    top="8%"
+                    left={{
+                        base: "35%",
+                        md: "65%",
+                    }}
                 >
-                    <Box
-                        borderRadius={2}
-                        border="solid white 1px"
-                        backgroundColor={"brand.cardBackgroundBlue"}
-                        height="100%"
-                        p={2}
-                        mx={8}
+                    <Text
+                        fontSize="3xl"
+                        fontWeight={500}
+                        textShadow={"1px 1px 1px black"}
+                        color="brand.bodyFont"
                     >
-                        <Stack
-                            height="100%"
-                            border="solid white 1px"
-                            p={4}
-                            flexDir={{
-                                base: "column",
-                                md: "row",
-                            }}
-                            alignItems={"center"}
-                            borderRadius={2}
-                            justify={"space-around"}
-                        >
-                            <Text fontSize="xl" color="brand.bodyFont">
-                                <Link href="#community">Community Events</Link>
-                            </Text>
-                            <Text fontSize="xl" color="brand.bodyFont">
-                                <Link href="#workshops">Workshops</Link>
-                            </Text>
-                            <Text fontSize="xl" color="brand.bodyFont">
-                                <Link href="#educational">
-                                    Educational Programming
-                                </Link>
-                            </Text>
-                        </Stack>
-                    </Box>
+                        WHAT WE OFFER
+                    </Text>
                 </Box>
+
                 <Image
                     width="100%"
                     height={"500px"}
                     objectFit={"cover"}
-                    src={"./wwe-1.jpeg"}
-                    alt="wwe-1"
+                    src={"./off-1-5.jpg"}
+                    alt="off-1"
                 />
-                <Box width="100%" height="150px"></Box>
+                <Box
+                    width="100%"
+                    p={{
+                        base: 8,
+                        md: 16,
+                    }}
+                    color="brand.bodyFont"
+                    display={"flex"}
+                    flexDir={"column"}
+                    justifyContent={"space-between"}
+                >
+                    <Text
+                        fontSize="xl"
+                        fontWeight={500}
+                        textShadow={"1px 1px 1px black"}
+                    >
+                        GWCD specializes in Chinese classical and folk dance.
+                        For an in-depth explanation of the styles included in
+                        this please continue reading.
+                    </Text>
+                    <Stack spacing={4} mt={32}>
+                        <Text fontSize="4xl" fontWeight={600}>
+                            Classical Chinese Dance
+                        </Text>
+                        <HStack
+                            flexWrap={"wrap"}
+                            textDecoration={"italic"}
+                            as="i"
+                            fontSize="2xl"
+                            fontWeight={300}
+                            spacing={{ base: 8, md: 16 }}
+                        >
+                            <Text>Dunhuang</Text>
+                            <Text>Peking Opera</Text>
+                            <Text>Han Tang</Text>
+                        </HStack>
+                        <Text fontSize="4xl" fontWeight={600}>
+                            Chinese Ethnic/Folk Dance
+                        </Text>
+                        <HStack
+                            flexWrap={"wrap"}
+                            as="i"
+                            fontSize="2xl"
+                            fontWeight={300}
+                            spacing={{ base: 8, md: 16 }}
+                        >
+                            <Text>Han</Text>
+                            <Text>Dai</Text>
+                            <Text>Inner Mongolian</Text>
+                            <Text>Tibetan</Text>
+                            <Text>Uyghur</Text>
+                            <Text>Miao</Text>
+                        </HStack>
+                    </Stack>
+                </Box>
             </Stack>
             <Stack
                 height="1000px"
@@ -67,83 +93,53 @@ export default function WhatWeOffer() {
                 spacing={0}
                 backgroundColor={"brand.cardBackgroundLightBlue"}
             >
-                <Box height={"500px"}>
-                    <Image
-                        width="100%"
-                        height={"500px"}
-                        objectFit={"cover"}
-                        src={"./wwe-2.jpeg"}
-                        alt="wwe-2"
-                    />
-
-                    <Box
-                        position={"absolute"}
-                        height={"500px"}
-                        width={"100%"}
-                        textShadow={"1px 1px 1px black"}
-                        p={8}
-                        margin="0 auto"
-                        color="brand.bodyFont"
-                        top="44%"
-                        display="flex"
-                        flexDir={"row"}
-                        justifyContent={"center"}
-                    >
-                        <Box
-                            width={"100%"}
-                            height="100%"
-                            maxWidth={"1200px"}
-                            display="flex"
-                            flexDir={"column"}
-                            justifyContent={"space-between"}
-                        >
-                            <Text
-                                fontSize="xl"
-                                fontWeight={500}
-                                textAlign={"right"}
-                            >
-                                GWCD is proud to offer customizable Chinese
-                                dance programs in three main areas
-                            </Text>
-                            <Text>Please contact us for more info </Text>
-                        </Box>
-                    </Box>
-                </Box>
-                <section id="educational">
+                <section id="dunhuang">
                     <Box
                         display="flex"
                         flexDir={{ base: "column", md: "row" }}
                         height={{ md: "500px" }}
+                        backgroundImage={"./off-2.jpeg"}
+                        backgroundRepeat={"no-repeat"}
+                        //   stretch background image to cover the entire container
+                        backgroundSize={"cover"}
                     >
                         <Image
                             height={"500px"}
-                            width={{ base: "100%", md: "calc(100% - 400px)" }}
+                            width={{
+                                md: "calc(100% - 400px)",
+                            }}
                             objectFit={"cover"}
-                            src={"./wwe-3.jpeg"}
+                            src={"./off-3.jpeg"}
                             alt="wwe-3"
+                            mx={{ base: 8, md: 0 }}
+                            my={{ base: 8, md: 0 }}
+                            ml={{
+                                md: 16,
+                            }}
+                            borderRadius={{ base: "lg", md: 0 }}
                         />
 
                         <Box
                             width={{ base: "100%", md: "400px" }}
-                            backgroundColor={"brand.cardBackgroundLightBlue"}
                             p={{ base: 8, md: 12 }}
+                            color={"brand.bodyFont"}
+                            fontWeight={500}
+                            textShadow={"1px 1px 1px black"}
                         >
-                            <Text fontSize="3xl" mb={4}>
-                                Educational Programs
+                            <Text fontSize="3xl" mb={4} textAlign={"center"}>
+                                Dunhuang
                             </Text>
-                            <Text>
-                                Designed for K-12 audiences as an introduction
-                                to China’s culture, language, and history
-                                through its dance. Our educational programs are
-                                highly customizable to match the themes,
-                                curriculum, and interactivity desired by each
-                                school. To request an educational showcase click
-                                here
+                            <Text textAlign={"left"}>
+                                Inspired by ancient Buddhist cave paintings of
+                                northwestern China, Dunhuang combines rhythmic
+                                techniques with classical Chinese dance to
+                                create a style that is invigorating, moving, and
+                                awe-inspiring.
                             </Text>
                         </Box>
                     </Box>
                 </section>
-                <section id="community">
+                <section id="peking">
                     <Box
                         display="flex"
                         flexDir={{ base: "column-reverse", md: "row" }}
@@ -155,17 +151,14 @@ export default function WhatWeOffer() {
                             p={{ base: 8, md: 12 }}
                         >
                             <Text fontSize="3xl" mb={4}>
-                                Community Events
+                                Peking Opera Inspired
                             </Text>
                             <Text>
-                                Celebrate Lunar New Year, AAPI Heritage Month,
-                                and more with a spectacular Chinese dance
-                                showcase by GWCD. We have partnered with
-                                countless community venues around the greater DC
-                                area to produce one-of-a-kind programs including
-                                interactive dance sections and informative
-                                spoken presentations To request a community
-                                showcase click here
+                                This style honors the style and techniques of
+                                Beijing Opera, one of China’s most recognizable
+                                artistic treasures. These dances are typically
+                                playful and vibrant and often utilize martial
+                                arts and gymnastics skills.
                             </Text>
                         </Box>
                         <Image
@@ -175,39 +168,6 @@ export default function WhatWeOffer() {
                             src={"./wwe-4.jpeg"}
                             alt="wwe-4"
                         />
-                    </Box>
-                </section>
-                <section id="workshops">
-                    <Box
-                        display="flex"
-                        flexDir={{ base: "column", md: "row" }}
-                        height={{ md: "500px" }}
-                    >
-                        <Image
-                            height={"500px"}
-                            width={{ base: "100%", md: "calc(100% - 400px)" }}
-                            objectFit={"cover"}
-                            src={"./wwe-5.jpeg"}
-                            alt="wwe-5"
-                        />
-                        <Box
-                            width={{ base: "100%", md: "400px" }}
-                            backgroundColor={"brand.cardBackgroundLightBlue"}
-                            p={{ base: 8, md: 12 }}
-                        >
-                            <Text fontSize="3xl" mb={4}>
-                                Workshops
-                            </Text>
-                            <Text>
-                                A once in a lifetime opportunity for both
-                                dancers and non-dancers to learn from GWCD’s
-                                masterful instructors. Participants in our
-                                workshops learn the basic technique of one or
-                                more classical or ethnic Chinese dance styles.
-                                To request a Chinese dance workshop click here
-                                To see a list of upcoming workshops click here
-                            </Text>
-                        </Box>
                     </Box>
                 </section>
             </Stack>

b11280850f7e28427dc9077cb0b5f7cd19aead87
IdleScV <weschen1996@gmail.com>
2023-10-15 02:45:47 -0400

    fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -48,7 +48,11 @@ export default function ContactForm() {
 
         try {
             const response = await fetch("/api/contact", {
-                /* ... */
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
             });
 
             if (!response.ok)
--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -16,17 +16,58 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             message,
         } = req.body;
 
+        const emailContent = `
+            <html>
+                <head>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f4f4f4;
+                            color: #333;
+                            margin: 0;
+                            padding: 0;
+                        }
+                        .content {
+                            background-color: #ffffff;
+                            margin: auto;
+                            width: 60%;
+                            padding: 20px;
+                            border-radius: 8px;
+                            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                            margin-top: 50px;
+                        }
+                        h1 {
+                            color: #2C3E50;
+                            margin-bottom: 20px;
+                        }
+                        p {
+                            margin-bottom: 10px;
+                        }
+                        .label {
+                            font-weight: bold;
+                            color: #2C3E50;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class="content">
+                        <h1>Performance Request from ${name}</h1>
+                        <p><span class="label">Name:</span> ${name}</p>
+                        <p><span class="label">Organization:</span> ${organization}</p>
+                        <p><span class="label">Requesting Performance:</span> ${isRequestingPerformance}</p>
+                        <p><span class="label">Performance Date:</span> ${performanceDate}</p>
+                        <p><span class="label">Message:</span></p>
+                        <p>${message}</p>
+                    </div>
+                </body>
+            </html>
+        `;
+
         const data = {
-            from: "GWCD <sender@example.com>", // Replace with your sender email
+            from: "GWCD Contact Page <GWCD.com>", // Replace with your sender email
             to: "weschen1996@gmail.com", // Replace with your recipient email
             subject: `Performance Request from ${name}`,
-            text: `
-                Name: ${name}
-                Organization: ${organization}
-                Requesting Performance: ${isRequestingPerformance}
-                Performance Date: ${performanceDate}
-                Message: ${message}
-            `,
+            html: emailContent,
         };
 
         mg.messages().send(data, function (error, body) {

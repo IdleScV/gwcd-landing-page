84123c0a877d99df56ca5839c8492a950ac1aabe
IdleScV <weschen1996@gmail.com>
2023-10-15 02:41:45 -0400

    ?

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -15,30 +15,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             performanceDate,
             message,
         } = req.body;
-        //    const emailContent = `
-        //     <html>
-        //         <head>
-        //             <style>
-        //                 body {font-family: Arial, sans-serif;}
-        //                 .content {margin: auto; width: 50%; padding: 10px;}
-        //             </style>
-        //         </head>
-        //         <body>
-        //             <div class="content">
-        //                 <h1>Performance Request from ${name}</h1>
-        //                 <p><strong>Name:</strong> ${name}</p>
-        //                 <p><strong>Organization:</strong> ${organization}</p>
-        //                 <p><strong>Requesting Performance:</strong> ${isRequestingPerformance}</p>
-        //                 <p><strong>Performance Date:</strong> ${performanceDate}</p>
-        //                 <p><strong>Message:</strong></p>
-        //                 <p>${message}</p>
-        //             </div>
-        //         </body>
-        //     </html>
-        // `;
 
         const data = {
-            from: "GWCD Contact Page <help@gwcd.com>", // Replace with your sender email
+            from: "GWCD <sender@example.com>", // Replace with your sender email
             to: "weschen1996@gmail.com", // Replace with your recipient email
             subject: `Performance Request from ${name}`,
             text: `
